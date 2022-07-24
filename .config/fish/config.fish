8be6f78d86bcf8387b8870180e76e6397d164894
hineni26 <ahanmondal67@gmail.com>
2022-07-25 00:43:30 +0530

    Removed unneeded bug fixes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -198,13 +198,3 @@ alias rr='curl -s -L https://raw.githubusercontent.com/keroserene/rickrollrc/mas
 
 ### SETTING THE STARSHIP PROMPT ###
 starship init fish | source
-
-### FLATPAK FISH BUG FIX ###
-set -l xdg_data_home $XDG_DATA_HOME ~/.local/share
-set -gx --path XDG_DATA_DIRS $xdg_data_home[1]/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share
-
-for flatpakdir in ~/.local/share/flatpak/exports/bin /var/lib/flatpak/exports/bin
-    if test -d $flatpakdir
-        contains $flatpakdir $PATH; or set -a PATH $flatpakdir
-    end
-end
